8ae15a453448ebdcf7bd2ba692e2b9656aff5265
mori <hanao_m1@ybb.ne.jp>
2025-11-09 11:50:12 +0900

    after stash

--- src/main/java/com/example/helloworld/helloworld.java
+++ src/main/java/com/example/helloworld/helloworld.java
@@ -4,5 +4,6 @@ public class helloworld {
 	public static void main(String[] args) {
 	// "Hello World" をコンソールに出力
 	System.out.println("Hello World");
+	System.out.println("Hello World");//after stash
 	}
 }
