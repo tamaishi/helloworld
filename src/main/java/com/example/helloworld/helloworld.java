4953c896dd17717a32bd0f91d0714ea68f6761dc
mori <hanao_m1@ybb.ne.jp>
2025-11-09 12:05:37 +0900

    test

--- src/main/java/com/example/helloworld/helloworld.java
+++ src/main/java/com/example/helloworld/helloworld.java
@@ -4,6 +4,6 @@ public class helloworld {
 	public static void main(String[] args) {
 	// "Hello World" をコンソールに出力
 	System.out.println("Hello World");
-	System.out.println("Hello World");//after stash
+	System.out.println("Hello World");//after stash ...
 	}
 }
